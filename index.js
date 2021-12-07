a1c0eaba6ab8ad610a4582691eba6239992c580c
Susana Marquina <smarq86@gmail.com>
2021-12-06 20:20:34 -0800

    created save button

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
+let countEl = document.getElementById('count-el');
 let count = 0;
 
 function increment() {
   count = count + 1;
-  console.log(count);
+  countEl.innerText = count;
 }
-increment();
+
+function save() {
+  console.log(count)
+}
